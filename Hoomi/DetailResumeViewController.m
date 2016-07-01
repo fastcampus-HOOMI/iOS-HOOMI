ccf68e74b3d3db5264f14ad9fed115f61a3d92ef
배지영 <melissaeliza0@gmail.com>
2016-07-01 23:56:28 +0900

    이력서 상세 화면 클래스 생성

---
+++ Hoomi/DetailResumeViewController.m
@@ -0,0 +1,27 @@
+//
+//  DetialResumeViewController.m
+//  Hoomi
+//
+//  Created by 배지영 on 2016. 7. 1..
+//  Copyright © 2016년 Jyo. All rights reserved.
+//
+
+#import "DetailResumeViewController.h"
+
+@interface DetailResumeViewController ()
+
+@end
+
+@implementation DetailResumeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"이곳은 이력서 목록을 누른 후, Detail 페이지가 나오는 곳");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
